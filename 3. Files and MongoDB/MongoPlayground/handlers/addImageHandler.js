808daf8e3d729e688b32042f0553c140cafb45aa
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-01-29 14:51:48 +0200

    Implemented Search Functionality.

--- 3. Files and MongoDB/MongoPlayground/handlers/addImageHandler.js
+++ 3. Files and MongoDB/MongoPlayground/handlers/addImageHandler.js
@@ -1,4 +1,5 @@
-
+const formidable = require('formidable');
+const Image = require('mongoose').model('Image');
 
 module.exports = (req, res) => {
   if (req.pathname === '/addImage' && req.method === 'POST') {
@@ -8,4 +9,46 @@ module.exports = (req, res) => {
   } else {
     return true
   }
+}
+
+function addImage(req, res) {
+  let form = formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    let imageUrl = fields.imageUrl;
+    let description = fields.description;
+    let title = fields.imageTitle;
+
+    let tags = fields.tagsId.split(',').reduce((arr, curVal) => {
+      if (arr.includes(curVal) || curVal.length === 0) {
+        return arr;
+      } else {
+        arr.push(curVal);
+        return arr;
+      }
+    }, []);
+
+
+    Image
+      .create({
+        url: imageUrl,
+        description: description,
+        title: title,
+        tags: tags
+      })
+      .then(() => {
+        res.writeHead(302, {
+          location: '/'
+        });
+        res.end();
+      })
+      .catch((err) => {
+        console.log(err.errors);
+      });
+
+  });
 }
