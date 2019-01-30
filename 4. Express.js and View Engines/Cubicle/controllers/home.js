0c97952f558972a3b56c99aaeed29e9e17158abb
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-01-30 22:47:28 +0200

    Added Searching.

--- 4. Express.js and View Engines/Cubicle/controllers/home.js
+++ 4. Express.js and View Engines/Cubicle/controllers/home.js
@@ -2,12 +2,35 @@ const Cube = require('../models/Cube');
 
 module.exports = {
     homeGet: (req, res) => {
-        res.render('index');
+        Cube
+            .find()
+            .select('_id name imageUrl difficulty')
+            .sort('-difficulty')
+            .then((cubes) => {
+
+                res.render('index', { cubes });
+            })
+            .catch((e) => console.log(e));
     },
     about: (req, res) => {
         res.render('about');
     },
     search: (req, res) => {
-        
+        let name = req.query.name;
+        let from = Number(req.query.form);
+        let to = Number(req.query.to);
+
+        if (name && from && to) {
+            Cube.find({})
+                .where('difficulty')
+                .gte(from)
+                .lte(to)
+                .then((cubes) => {
+                    let filtered = cubes
+                        .filter(c => c.name.toLowerCase().includes(name.toLowerCase()));
+
+                    res.render('index', { cubes: filtered });
+                });
+        }
     }
 }
